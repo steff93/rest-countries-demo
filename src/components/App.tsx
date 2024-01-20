648ebf9a86031bc9f1e253add82fa2b3d0363a7d
stefanB <stefan_boghean@yahoo.com>
2024-01-20 16:06:16 +0200

    added light/dark themes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Header themeName="Dark Mode" />
+      <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Header.scss";
 
-interface HeaderProps {
-  themeName: "Light Mode" | "Dark Mode";
-}
+type Theme = "dark" | "light";
 
-const Header = ({ themeName }: HeaderProps) => {
+const Header = () => {
+  const [theme, setTheme] = useState<Theme>("dark");
+  const themeName = theme === "dark" ? "Light Mode" : "Dark Mode";
+
+  const handleThemeChange = () => {
+    setTheme((currentTheme) => (currentTheme === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.querySelector("body")?.classList.add("light");
+    } else {
+      document.querySelector("body")?.classList.remove("light");
+    }
+  }, [theme]);
+
   return (
     <div className="header">
       <div>Where in the world?</div>
-      <div className="theme-switcher">{themeName}</div>
+      <button className="theme-switcher" onClick={handleThemeChange}>
+        {themeName}
+      </button>
     </div>
   );
 };
