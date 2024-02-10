c85f60c9bd384de91ddf8b00325ece762cbd93be
stefanB <stefan_boghean@yahoo.com>
2024-02-10 18:20:13 +0200

    Search styling, added clearSearch handler

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,49 @@
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import "./Search.scss";
-
-import React, { useRef } from "react";
 
 interface SearchProps {
   onSearchSubmit: (searchValue: string) => void;
 }
 
 export const Search = ({ onSearchSubmit }: SearchProps) => {
-  const searchValue = useRef("");
+  const [searchValue, setSearchValue] = useState("");
+  const isAnythingInSearch = !!searchValue.length;
+  const placeholderText = "Search for a country...";
+  const inputIcon = isAnythingInSearch ? faXmark : faMagnifyingGlass;
+  const clearSearchClass = isAnythingInSearch ? "search__icon--clear" : "";
 
   const handleChange = (e: React.ChangeEvent) => {
-    searchValue.current = (e.target as HTMLInputElement).value;
+    setSearchValue((e.target as HTMLInputElement).value);
+  };
+
+  const handleSearchClear = () => {
+    if (!isAnythingInSearch) return;
+
+    setSearchValue("");
+    onSearchSubmit("");
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    onSearchSubmit(searchValue.current);
+    onSearchSubmit(searchValue);
   };
 
   return (
     <form className="search" onSubmit={handleSubmit}>
-      <input type="search" onChange={handleChange} />
+      <FontAwesomeIcon
+        icon={inputIcon}
+        className={`search__icon ${clearSearchClass}`}
+        onClick={handleSearchClear}
+      />
+      <input
+        type="search"
+        value={searchValue}
+        onChange={handleChange}
+        placeholder={placeholderText}
+      ></input>
     </form>
   );
 };
