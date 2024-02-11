b45c23f5e48de58f896a647e4ccb752494ddb519
stefanB <stefan_boghean@yahoo.com>
2024-02-11 20:52:36 +0200

    Styled sort select

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,10 +1,8 @@
-// Accept a calback prop
-// Accept a regions props
-// Structure:  Simulate a select with a div, list regions below
-// Default value
-
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Region } from "../../types";
+import "./Sort.scss";
 
 interface SortProps {
   onSort: (region: Region) => void;
@@ -13,32 +11,51 @@ interface SortProps {
 
 const Sort = ({ onSort, regions }: SortProps) => {
   const [selectedRegion, setSelectedRegion] = useState<Region | null>(null);
-  const defaultValue = "Filter By Region";
+  const [sortExpanded, setSortExpanded] = useState(false);
+  const defaultValue = "Filter by Region";
   const highlightedValue =
     selectedRegion && selectedRegion !== "All" ? selectedRegion : defaultValue;
 
+  const expandedClass = sortExpanded ? "sort__select--expanded" : "";
   if (!regions.includes("All")) {
     regions.push("All");
   }
 
-  const handleClick = (region: Region) => {
+  const handleSortSelect = (region: Region) => {
     setSelectedRegion(region);
+    setSortExpanded(false);
 
     onSort(region);
   };
 
+  const handleSortToggle = () => {
+    setSortExpanded((prev) => !prev);
+  };
+
   return (
-    <div>
-      <div>{highlightedValue}</div>
-      <ul>
-        {regions.map((region, index) => {
-          return (
-            <li key={index} onClick={() => handleClick(region)}>
-              {region}
-            </li>
-          );
-        })}
-      </ul>
+    <div className="sort">
+      <div
+        className={`sort__select ${expandedClass}`}
+        onClick={handleSortToggle}
+      >
+        <span className="sort__selected">{highlightedValue}</span>
+        <FontAwesomeIcon className="sort__icon" icon={faChevronUp} />
+      </div>
+      {sortExpanded && (
+        <ul className="sort__regions">
+          {regions.map((region, index) => {
+            return (
+              <li
+                className="sort__region"
+                key={index}
+                onClick={() => handleSortSelect(region)}
+              >
+                {region}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
