757e2feda7c2a03d4c957c73f4bd5cd544f58425
stefanB <stefan_boghean@yahoo.com>
2024-04-27 21:30:04 +0300

    working on single country view

--- src/components/Single/Single.tsx
+++ src/components/Single/Single.tsx
@@ -1,17 +1,32 @@
 import { CountryData } from "../../types";
 import "./Single.scss";
 
-interface SingleProps {
-  countryFlag: string;
-  countryName: string;
-  additionalData: Partial<CountryData>;
+interface CountryDetailProps {
+  countryData: CountryData;
 }
 
 const sectionClass = "country-single";
 
-const Single = (props: SingleProps) => {
-  const { additionalData, countryName } = props;
-  const flag = additionalData.flags?.png;
+const CountryDetail = ({ countryData }: CountryDetailProps) => {
+  const { flags, name } = countryData;
+  const flag = flags?.png;
+  const countryName = name.common;
+  const nativeName = Object.values(name.nativeName)[0].common;
+  const {
+    population,
+    region,
+    capital,
+    cca2: domainName,
+    currencies,
+    languages,
+    borders,
+  } = countryData;
+
+  const currenciesList = Object.values(currencies).reduce((acc, currency) => {
+    return [...acc, currency.name];
+  }, [] as string[]);
+
+  console.log(borders);
 
   return (
     <div className={sectionClass}>
@@ -22,24 +37,54 @@ const Single = (props: SingleProps) => {
           <img src={flag} alt={`${countryName}-flag`} />
         </div>
 
-        <div className="country-single__data">
-          <h3 className=""></h3>
-          {/**
-           * NAme
-           *
-           * Native Name
-           * Population
-           * Region
-           * Sub Region
-           * Capital
-           * Top Level Domain
-           * Currencies
-           * Languages
-           */}
+        <div className={`${sectionClass}__data`}>
+          <h3 className={`${sectionClass}__name`}>{countryName}</h3>
+
+          <div className={`${sectionClass}__details`}>
+            <div className={`${sectionClass}__details--left`}>
+              <h5 className={`${sectionClass}__native-name`}>
+                <b>Native Name:</b> {nativeName}
+              </h5>
+
+              <h5 className={`${sectionClass}__population`}>
+                <b>Population:</b> {population}
+              </h5>
+
+              <h5 className={`${sectionClass}__region`}>
+                <b>Region:</b> {region}
+              </h5>
+
+              <h5 className={`${sectionClass}__capital`}>
+                <b>Capital:</b> {capital?.length ? capital[0] : "N/A"}
+              </h5>
+            </div>
+            <div className={`${sectionClass}__details--right`}>
+              <h5 className={`${sectionClass}__domain`}>
+                <b>Top Level Domain:</b> .{domainName.toLowerCase()}
+              </h5>
+              <h5 className={`${sectionClass}__currency`}>
+                <b>Currencies:</b> {currenciesList.join(", ")}
+              </h5>
+              <h5 className={`${sectionClass}__language`}>
+                <b>Languages:</b> {Object.values(languages).join(", ")}
+              </h5>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${sectionClass}__borders`}>
+          <span>Border Countries</span>
+          {borders.map((border, index) => {
+            return (
+              <div className={`${sectionClass}__border`} key={index}>
+                {border}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 };
 
-export default Single;
+export default CountryDetail;
